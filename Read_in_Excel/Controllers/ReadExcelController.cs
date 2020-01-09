d81b9ddfc61aaebfaa2f92d902859587a60b0889
SarvarbekM <sarvarbek.matvapayev@gmail.com>
2020-01-10 00:26:21 +0500

    first commit

---
+++ Read_in_Excel/Controllers/ReadExcelController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Windows.Forms;
+using Read_in_Excel.Models;
+using System.IO;
+
+namespace Read_in_Excel.Controllers
+{
+    public class ReadExcelController : Controller
+    {
+        ReadExcelModel ob = new ReadExcelModel();
+        //
+        // GET: /ReadExcel/
+        //
+        public ActionResult Index()
+        {
+            // OpenFileDialog opendialog = new OpenFileDialog();
+            //opendialog.ShowDialog();        
+            //if (opendialog.ShowDialog() == DialogResult.OK)
+            //{
+            //    //ob.Get_Values_in_Excel(opendialog.FileName);
+            // //   ob.Get_Values_in_Excel(@"C:\Users\$ARVARBEK\Desktop\Shablon.xlsx");
+            //    MessageBox.Show("satr=" + ob.satr);
+            //    MessageBox.Show("ustun=" + ob.ustun);
+            //}
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index (HttpPostedFileBase file)
+        {
+            //var files = Request.Files[0];
+            //MessageBox.Show("Ura1");
+            //MessageBox.Show(files.ContentLength.ToString());
+            //if(file!=null && file.ContentLength>0)
+            //{
+            //    MessageBox.Show("Ura2");
+            //    MessageBox.Show(Path.GetFileName(file.FileName));
+            //    var filename = Path.GetFileName(file.FileName);
+            //    var path = Path.Combine(Server.MapPath("~/Images/"), filename);
+            //    file.SaveAs(path);
+            //}
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult Index1()
+        {
+            var files = Request.Files[0];
+            
+            MessageBox.Show("Ura1");            
+            if (files != null && files.ContentLength > 0)
+            {
+                MessageBox.Show("Ura2");
+                ob.Read();
+                var filename = Path.GetFileName(files.FileName);
+                var path = Path.Combine(Server.MapPath("~/Images/"), filename);
+                //files.SaveAs(path);
+            }
+            return RedirectToAction("Index");
+        }
+
+        
+    }
+}
---
+++ Read_in_Excel/Models/ReadExcelModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+
+namespace Read_in_Excel.Models
+{
+    public class ReadExcelModel
+    {
+
+        public void Read()
+        {
+            Microsoft.Office.Interop.Excel.Application _excelApp = new Microsoft.Office.Interop.Excel.Application();
+            //_excelApp.Visible = true;
+            String filename = @"C:\Users\$ARVARBEK\Desktop\Shablon.xlsx";
+            Workbook workbook = _excelApp.Workbooks.Open(filename);
+            Worksheet worksheet = (Worksheet)workbook.Worksheets[1];
+            Range excelRange = worksheet.UsedRange;
+            object[,] valueArray = (object[,])excelRange.get_Value(XlRangeValueDataType.xlRangeValueDefault);
+            try
+            {
+                for (int row = 1; row <= worksheet.UsedRange.Rows.Count; ++row)
+                {
+                    for (int col = 1; col <= worksheet.UsedRange.Columns.Count; ++col)
+                    {
+                        valueArray[row, col].ToString();
+                    }
+                }
+            }
+            catch (Exception) { }
+            workbook.Close(false);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
+            _excelApp.Quit();
+            System.Runtime.InteropServices.Marshal.FinalReleaseComObject(_excelApp);            
+        }        
+    }
+}
